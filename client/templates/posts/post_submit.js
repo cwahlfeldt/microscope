203326ef41378ba01cb1551df051be0cae9b2ffe
Chris Wahlfeldt <chriswahlfeldt@gmail.com>
2015-04-23 13:36:02 -0500

    chapter 7

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,25 @@
+Template.postSubmit.events({
+    'submit form': function(e) {
+        e.preventDefault();
+
+        var post = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val()
+        };
+
+        Meteor.call('postInsert', post, function(error, result) {
+            // display the error to the user and abort
+            if (error) {
+                return alert(error.reason);
+            }
+
+            if (result.postExists) {
+                alert('Someone else created this post already!');
+            }
+
+            Router.go('postPage', {
+                _id: result._id
+            });
+        });
+    }
+});
