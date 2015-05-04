3f0a0e8067d262a9410237408984f4ba02ba677a
illinismiles <chriswahlfeldt@gmail.com>
2015-05-04 07:51:48 -0500

    chapter 10

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -2,7 +2,7 @@ Template.postItem.helpers({
     ownPost: function() {
         return this.userId === Meteor.userId();
     },
-    domain: function () {
+    domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
         return a.hostname;
